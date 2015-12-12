9458294e6582a532c5a6250e6ebd5bf3a1b4b65b
nerdfiles <nerdfiles@gmail.com>
2015-12-11 19:27:05 -0600

    Basic scraper init.

---
+++ index.js
@@ -0,0 +1,70 @@
+var artoo = require('artoo-js');
+var cheerio = require('cheerio');
+var request = require('request');
+var Q = require('q');
+
+var params = null;
+var _xmlString = '<ul><li>h';
+
+function load_document (config) {
+  var def = Q.defer();
+  var defaultConfig = {
+    url: null
+  }
+  var _config = config || defaultConfig;
+  if (!_config.url)
+    return;
+  request(_config.url, function (error, response, html) {
+    if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(html);
+      def.resolve($);
+
+      $('span.comhead').each(function(i, element){
+        var a = $(this).prev();
+        console.log(a.text());
+      });
+    }
+  });
+  return def.promise;
+}
+
+function scrape_document (config) {
+  var defaultConfig = {
+    strategy : 'artoo',
+    content  : '',
+    selector : '',
+    params   : params
+  };
+  var _config = config || defaultConfig;
+  var data = null;
+  var $ = null;
+
+  if (_config.strategy !== 'artoo') {
+    artoo.bootstrap(cheerio);
+    $ = cheerio.load(_config.content);
+    data = $(_config.selector).scrape(_config.params);
+  } else {
+    $ = cheerio.load(_config.content);
+    artoo.setContext($);
+    data = artoo.scrape(_config.selector, _config.params);
+  }
+
+  return data;
+
+}
+
+var domainConfig = {
+  url: 'https://news.ycombinator.com'
+};
+load_document(domainConfig).then(function (html) {
+  var __xmlString = html || _xmlString;
+  var conf = {
+    strategy : 'artoo',
+    content  : __xmlString,
+    selector : 'ul > li',
+    params   : null
+  };
+  console.dir(conf);
+
+  console.log(scrape_document(conf));
+})
